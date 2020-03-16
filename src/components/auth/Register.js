2b8f390d77190a0224e27b5ca6e0ccbb5d11f6f4
Lauren Riddle <lari7132@gmail.com>
2020-03-16 17:25:17 -0500

    added profile pic upload to register page

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -6,6 +6,8 @@ import { register, isAuthenticated } from "../../modules/SimpleAuth"
 import APIManager from "../../modules/APIManager"
 import './Auth.css'
 import { Button } from "react-bootstrap"
+import { cloudName, uploadPreset } from '../../modules/Credentials';
+import "../profile/Profile.css"
 
 class Register extends Component {
 
@@ -94,6 +96,21 @@ class Register extends Component {
         }
     }
 
+    openCloudinaryWidget = () => {
+        let widget = window.cloudinary.createUploadWidget({
+            cloudName: cloudName,
+            uploadPreset: uploadPreset
+        }, (error, result) => {
+            if (!error && result && result.event === "success") {
+                this.setState({
+                    image: result.info.url
+                })
+            }
+        }
+        )
+        widget.open();
+    }
+
 
     render() {
 
@@ -180,7 +197,14 @@ class Register extends Component {
                         placeholder="Confirmation Password"
 
                     />
+                    
+                    {this.state.image !== null &&
+                        <img src={this.state.image} alt="user" className="pre-profile-img"></img>
+                    }
 
+                    <div className="upload_widget_container">
+                        <Button type="button" id="upload_widget" className="cloudinary-button" onClick={this.openCloudinaryWidget}>Choose File</Button>
+                    </div>
 
                     <Button type="submit">
                         Register
