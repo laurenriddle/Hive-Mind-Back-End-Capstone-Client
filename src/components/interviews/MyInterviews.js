1b73529fd750960fd31c4f92aac583329bff6b69
Lauren Riddle <lari7132@gmail.com>
2020-03-16 16:53:56 -0500

    fixed interviews not re-rendering after delete on my interviews page

--- src/components/interviews/MyInterviews.js
+++ src/components/interviews/MyInterviews.js
@@ -48,12 +48,18 @@ class MyInterviews extends Component {
             // make a DELETE request to the DB for the selected interview
             APIManager.delete("interviews", id)
                 .then(() => {
-                    // get all interviews again
+                    // gets all interviews for the specific user
                     APIManager.getAllAuth("interviews?applicant=true")
                         .then((interviews) => {
-                            // update state with interviews
+                            let companies = []
+                            interviews.map((interview) => {
+                                // if the company is not already in the companies array, put it in there
+                                this.pushEntry(companies, interview.company)
+                            })
+                            // sets the interviews and companies in state
                             this.setState({
-                                interviews: interviews
+                                interviews: interviews,
+                                companies: companies
                             })
                         })
 
@@ -64,12 +70,12 @@ class MyInterviews extends Component {
     filterInterviews = (id) => {
         // gets all interviews for the company that was picked
         APIManager.getAllAuth(`interviews?applicant=true&&company=${id}`)
-        .then((interviews) => {
-            // sets the interviews in state
-            this.setState({
-                interviews: interviews
+            .then((interviews) => {
+                // sets the interviews in state
+                this.setState({
+                    interviews: interviews
+                })
             })
-        })
     }
 
     getAllInterviews = () => {
@@ -92,15 +98,19 @@ class MyInterviews extends Component {
 
         return (
             <>
-                <label>Filter by Company:</label>
-                <ButtonToolbar aria-label="Toolbar with button groups">
-                    <ButtonGroup className="mr-2" aria-label="First group">
-                    <Button onClick={() => this.getAllInterviews()}>All</Button>
-                        {this.state.companies.map((company) => {
-                            return <Button onClick={() => this.filterInterviews(company.id)}>{company.name}</Button>
-                        })}
-                    </ButtonGroup>
-                </ButtonToolbar>
+                {this.state.companies.length > 0 &&
+                    <>
+                        <label>Filter by Company:</label>
+                        <ButtonToolbar aria-label="Toolbar with button groups">
+                            <ButtonGroup className="mr-2" aria-label="First group">
+                                <Button onClick={() => this.getAllInterviews()}>All</Button>
+                                {this.state.companies.map((company) => {
+                                    return <Button onClick={() => this.filterInterviews(company.id)}>{company.name}</Button>
+                                })}
+                            </ButtonGroup>
+                        </ButtonToolbar>
+                    </>
+                }
                 <Button onClick={() => this.props.history.push('/interview/new')}>+ New</Button>
                 {this.state.interviews.map((interview) => {
                     return <InterviewCard {...this.props} key={interview.id} interview={interview} deleteInterview={this.deleteInterview} />
