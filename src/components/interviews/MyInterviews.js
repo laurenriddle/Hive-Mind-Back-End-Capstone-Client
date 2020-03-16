f727c134036e7fe1e24729d3787c546a4db8c782
Lauren Riddle <lari7132@gmail.com>
2020-03-16 16:39:40 -0500

    added interview filter

--- src/components/interviews/MyInterviews.js
+++ src/components/interviews/MyInterviews.js
@@ -3,7 +3,7 @@
 import React, { Component } from "react"
 import { Link } from 'react-router-dom'
 import APIManager from '../../modules/APIManager'
-import { Button } from 'react-bootstrap'
+import { Button, ButtonGroup, ButtonToolbar } from 'react-bootstrap'
 import InterviewCard from "./InterviewCard"
 class MyInterviews extends Component {
     state = {
@@ -17,23 +17,29 @@ class MyInterviews extends Component {
         this.setState(stateToChange)
     }
 
+    // this function searches an array to make sure the object does not already exist
+    pushEntry = (array, item) => {
+        // if the object does not already exist in the array, push the object into the array
+        if (!array.find(({ id }) => id === item.id)) {
+            array.push(item);
+        }
+    }
+
     componentDidMount() {
         // gets all interviews for the specific user
         APIManager.getAllAuth("interviews?applicant=true")
             .then((interviews) => {
-                // sets the interviews in state
+                let companies = []
+                interviews.map((interview) => {
+                    // if the company is not already in the companies array, put it in there
+                    this.pushEntry(companies, interview.company)
+                })
+                // sets the interviews and companies in state
                 this.setState({
-                    interviews: interviews
+                    interviews: interviews,
+                    companies: companies
                 })
             })
-        // Gets all companies to load filter system
-        APIManager.getAllAuth("companies")
-        .then((companies) => {
-            // sets companies in state
-            this.setState({
-                companies: companies
-            })
-        })
     }
 
     deleteInterview = (id) => {
@@ -55,11 +61,46 @@ class MyInterviews extends Component {
         }
     }
 
+    filterInterviews = (id) => {
+        // gets all interviews for the company that was picked
+        APIManager.getAllAuth(`interviews?applicant=true&&company=${id}`)
+        .then((interviews) => {
+            // sets the interviews in state
+            this.setState({
+                interviews: interviews
+            })
+        })
+    }
+
+    getAllInterviews = () => {
+        APIManager.getAllAuth("interviews?applicant=true")
+            .then((interviews) => {
+                let companies = []
+                interviews.map((interview) => {
+                    // if the company is not already in the companies array, put it in there
+                    this.pushEntry(companies, interview.company)
+                })
+                // sets the interviews and companies in state
+                this.setState({
+                    interviews: interviews,
+                    companies: companies
+                })
+            })
+    }
 
     render() {
 
         return (
             <>
+                <label>Filter by Company:</label>
+                <ButtonToolbar aria-label="Toolbar with button groups">
+                    <ButtonGroup className="mr-2" aria-label="First group">
+                    <Button onClick={() => this.getAllInterviews()}>All</Button>
+                        {this.state.companies.map((company) => {
+                            return <Button onClick={() => this.filterInterviews(company.id)}>{company.name}</Button>
+                        })}
+                    </ButtonGroup>
+                </ButtonToolbar>
                 <Button onClick={() => this.props.history.push('/interview/new')}>+ New</Button>
                 {this.state.interviews.map((interview) => {
                     return <InterviewCard {...this.props} key={interview.id} interview={interview} deleteInterview={this.deleteInterview} />
