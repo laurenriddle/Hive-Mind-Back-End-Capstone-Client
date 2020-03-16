992b75817220c359a073ad3034f5a8af767c2f4b
Lauren Riddle <lari7132@gmail.com>
2020-03-16 16:00:37 -0500

    completed profile update on user

--- src/components/profile/ProfileEditForm.js
+++ src/components/profile/ProfileEditForm.js
@@ -1,7 +1,6 @@
 // Purpose: To create the interview edit form and execute the logic associated 
 
 import React, { Component } from "react"
-import { Link } from 'react-router-dom'
 import APIManager from '../../modules/APIManager'
 import { Button, FormControl, FormLabel, Form } from 'react-bootstrap'
 class ProfileEditForm extends Component {
@@ -43,7 +42,7 @@ class ProfileEditForm extends Component {
                     aboutme: applicant[0].aboutme
                 })
             })
-        
+
         // gets all cohorts to populate the dropdown 
         APIManager.getAllAuth("cohorts")
             .then((cohorts) => {
@@ -56,47 +55,39 @@ class ProfileEditForm extends Component {
     }
 
     updateProfile = () => {
-        // defines the interview object that will be submitted to the DB
-        const interview = {
-            company_id: this.state.company,
-            offer: this.state.offer,
-            position: this.state.position,
-            date: this.state.date,
-            review: this.state.review,
-            advice: this.state.advice,
-            interview_type: this.state.interview_type,
-            in_person: this.state.in_person,
-            code_challege: this.state.code_challege,
-            id: this.props.match.params.interviewId
+        // defines the applicant object that will be submitted to the DB
+        const applicant = {
+            linkedin_profile: this.state.linkedin_profile,
+            employer: this.state.employer,
+            is_employed: this.state.is_employed,
+            email: this.state.email,
+            first_name: this.state.first_name,
+            last_name: this.state.last_name,
+            username: this.state.username,
+            cohort_id: this.state.cohort_id,
+            image: this.state.image,
+            aboutme: this.state.aboutme
         }
 
         // checks to see if the user filled out the entire form
-        if (this.state.company !== "" && this.state.offer !== "" && this.state.position !== "" && this.state.date !== "" && this.state.advice !== "" && this.state.interview_type !== "" && this.state.in_person !== "" && this.state.code_challege !== "") {
-            // makes a put to the interviews table
-            APIManager.update(`interviews`, interview)
+        if (this.state.first_name !== "" && this.state.last_name !== "" && this.state.email !== "" && this.state.username !== "" ) {
+            // makes a put to the applicants table
+            APIManager.update_profile("applicants", applicant)
                 .then(() => {
-                    // pushes the user to their my interviews page
-                    this.props.history.push("/myinterviews")
+                    // pushes the user to their my profile page
+                    this.props.history.push("/profile")
                 })
         } else {
-            // tiggers alerts if the user has not filled out a field in the form
-            if (this.state.company === "") {
-                alert('Please select a company.')
-            } else if (this.state.position === "") {
-                alert('Please tell us what position you interviewed for.')
-            } else if (this.state.date === "") {
-                alert('Please select a date.')
-            } else if (this.state.interview_type === "") {
-                alert('Please tell use what type of interview you had.')
-            } else if (this.state.in_person === "") {
-                alert('Please tell us if this interview was in person or online/video conference.')
-            } else if (this.state.code_challege === "") {
-                alert('Please tell us if there was a coding challenge in this interview.')
-            } else if (this.state.offer === "") {
-                alert('Please tell us if you received an offer as a result of this interview.')
-            } else if (this.state.advice === "") {
-                alert('Please enter some advice for fellow job hunters.')
-            }
+            // tiggers alerts if the user has not filled out a required field in the form
+            if (this.state.first_name === "") {
+                alert('Please enter a first name.')
+            } else if (this.state.last_name === "") {
+                alert('Please enter a last name.')
+            } else if (this.state.username === "") {
+                alert('Please enter a username.')
+            } else if (this.state.email === "") {
+                alert('Please enter an email.')
+            } 
         }
     }
     render() {
@@ -107,24 +98,28 @@ class ProfileEditForm extends Component {
                     <FormControl
                         value={this.state.first_name}
                         id="first_name"
+                        required
                         onChange={this.handleInputChange}
                         type="text"></FormControl>
 
                     <FormControl
                         value={this.state.last_name}
                         id="last_name"
+                        required
                         onChange={this.handleInputChange}
                         type="text"></FormControl>
 
                     <FormControl
                         value={this.state.username}
                         id="username"
+                        required
                         onChange={this.handleInputChange}
                         type="text"></FormControl>
 
                     <FormControl
                         value={this.state.email}
                         id="email"
+                        required
                         onChange={this.handleInputChange}
                         type="text"></FormControl>
 
@@ -173,20 +168,6 @@ class ProfileEditForm extends Component {
                         placeholder="If you are employed, where are you working?"
                         onChange={this.handleInputChange}
                         type="text"></FormControl>
-
-                    <FormControl
-                        value={this.state.review}
-                        id="review"
-                        placeholder="Please provide a brief review of this company (i.e. Tech Stack, Culture, etc)..."
-                        onChange={this.handleInputChange}
-                        as="textarea" rows="5" ></FormControl>
-
-                    <FormControl
-                        value={this.state.advice}
-                        id="advice"
-                        placeholder='Please provide advice for anyone interviewing with this company...'
-                        onChange={this.handleInputChange}
-                        as="textarea" rows="5" ></FormControl>
 
                 </Form>
                 <Button onClick={() => this.updateProfile()}>Save Changes</Button>
