327ba5db9145fa5dafc23e4c4cfd57de3ce09f9f
Lauren Riddle <lari7132@gmail.com>
2020-08-05 17:50:08 -0500

    added check to see if user is a friend

--- src/components/search/SearchUserCard.js
+++ src/components/search/SearchUserCard.js
@@ -3,16 +3,39 @@
 import { Card, Button } from "react-bootstrap";
 import React, {Component} from 'react'
 import { Link } from "react-router-dom";
-
+import APIManager from '../../modules/APIManager'
 
 class SearchUserCard extends Component {
+    state = {
+        isFriend: false,
+        friends: []
+        
+    }
+
+    componentDidMount() {
+        APIManager.getAllAuth("friends?applicant=True")
+        .then((friends) => {
+            console.log(this.props.applicant.id , "FRIENDS")
+
+            friends.forEach((friend) => {
+            console.log(friend, "FRIENDS")
+
+                if(this.props.applicant.id === friend.friend.user.id)
+                {
+                    this.setState({
+                        isFriend: true
+                    })
+                }
+            })
+        })
+    }
 
     render() {
         return (
             <>
             <Card className="company-results-card">
                 <Card.Title><img src={this.props.applicant.image} alt="profile" width="50" height="50" className="card-image"></img>  <Link className="view-interviews-link"  to={`/profile/${this.props.applicant.id}`}>{this.props.applicant.user.first_name} {this.props.applicant.user.last_name}</Link></Card.Title>
-                {this.props.friend ? <Button onClick={() => this.props.deleteFriend(this.props.applicant.id)}>Unfollow</Button> : <Button onClick={() => this.props.addNewFriend(this.props.applicant.id)} >Follow</Button>}
+                {this.state.isFriend ? <Button onClick={() => this.props.deleteFriend(this.props.applicant.id)}>Unfollow</Button> : <Button onClick={() => this.props.addNewFriend(this.props.applicant.id)} >Follow</Button>}
             </Card>
 
             </>
