af5a60ece637f2f4f7d552574b5ca3b08059fb76
Lauren Riddle <lari7132@gmail.com>
2020-08-06 18:09:56 -0500

    added styles

--- src/components/search/SearchUserCard.js
+++ src/components/search/SearchUserCard.js
@@ -14,7 +14,7 @@ class SearchUserCard extends Component {
             
             <Card className="company-results-card">
                 <Card.Title><img src={this.props.applicant.image} alt="profile" width="50" height="50" className="card-image"></img>  <Link className="view-interviews-link"  to={`/profile/${this.props.applicant.id}`}>{this.props.applicant.user.first_name} {this.props.applicant.user.last_name}</Link></Card.Title>
-                {this.props.isFriendList ? <Button onClick={() => this.props.deleteFriend(this.props.applicant.id)}>Unfollow</Button> : <></>}
+                {this.props.isFriendList ? <Button className="unfollow-button" onClick={() => this.props.deleteFriend(this.props.applicant.id)}>Unfollow</Button> : <></>}
          </Card>
 
             )
